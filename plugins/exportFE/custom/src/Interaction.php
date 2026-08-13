7cfcc3fcc65b8100b38ab5d18c3212d14d91975d
Sajo <75281007+sajotrei@users.noreply.github.com>
2026-08-13 20:38:58 +0200

    FE: add custom provider facade without core changes

---
+++ plugins/exportFE/custom/src/Interaction.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Plugins\ExportFE;
+
+use API\Services;
+use Plugins\ExportFE\Providers\ProviderFactory;
+use Plugins\ExportFE\Providers\ProviderInterface;
+
+class Interaction extends Services
+{
+    protected static function getProvider(): ProviderInterface
+    {
+        return ProviderFactory::make(ProviderFactory::OSMCLOUD);
+    }
+
+    public static function isEnabled()
+    {
+        return static::getProvider()->isEnabled();
+    }
+
+    public static function sendInvoice($id_record)
+    {
+        return static::getProvider()->sendInvoice((int) $id_record);
+    }
+
+    public static function getInvoiceRecepits($id_record)
+    {
+        return static::getProvider()->getInvoiceReceipts((int) $id_record);
+    }
+}
