56bd79e9e3e31d6380370c5f7bc570c66d0b66a1
Sajo <75281007+sajotrei@users.noreply.github.com>
2026-08-17 01:01:17 +0200

    fix: integra destinazioni nel lifecycle del modulo backup

---
+++ modules/backups/controller_before.php
@@ -0,0 +1,13 @@
+<?php
+
+/*
+ * OpenSTAManager: il software gestionale open source per l'assistenza tecnica e la fatturazione
+ * Copyright (C) DevCode s.r.l.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ */
+
+include __DIR__.'/sections/destinations.php';
