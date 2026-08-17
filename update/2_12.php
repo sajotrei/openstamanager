b0db1b8571f8c9b55222eec33da387cb7906c3ec
Sajo <75281007+sajotrei@users.noreply.github.com>
2026-08-17 13:51:17 +0200

    fix: allow multiple backup paths per adapter

--- update/2_12.php
+++ update/2_12.php
@@ -89,7 +89,7 @@ $dbo->query('CREATE TABLE IF NOT EXISTS `zz_backup_destinations` (
     `enabled` TINYINT(1) NOT NULL DEFAULT 1,
     `retention` INT(11) NOT NULL DEFAULT 10,
     PRIMARY KEY (`id`),
-    UNIQUE KEY `unique_backup_destination_adapter` (`id_adapter`),
+    UNIQUE KEY `unique_backup_destination_adapter_path` (`id_adapter`, `path`),
     KEY `idx_backup_destination_enabled` (`enabled`),
     CONSTRAINT `zz_backup_destinations_ibfk_1` FOREIGN KEY (`id_adapter`) REFERENCES `zz_storage_adapters` (`id`) ON UPDATE CASCADE ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4');
